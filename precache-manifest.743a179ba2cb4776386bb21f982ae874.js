860caa3c602ece0b9140e76fae55a077c188a28b
SadmanShafiq <saadmanshafiq@gmail.com>
2020-01-06 16:27:03 +1100

    Updates

---
+++ precache-manifest.743a179ba2cb4776386bb21f982ae874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad69d4ae6cccae549c3d27cfa9b8c101",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4b2fcaf8d01353538600",
+    "url": "/portfolio/static/js/2.bfe2ba85.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/portfolio/static/js/2.bfe2ba85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1030ff91c2807ff592e",
+    "url": "/portfolio/static/js/main.be6ee453.chunk.js"
+  },
+  {
+    "revision": "3abfa529fd29fcbb7e75",
+    "url": "/portfolio/static/js/runtime-main.27d27bbe.js"
+  }
+]);
